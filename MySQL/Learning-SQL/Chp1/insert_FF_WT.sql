532f48599a84c38d558111ecfdaa1a1bfdd8a7e3
smurphy123 <smurphysmail@gmail.com>
2021-04-24 11:51:56 -0400

    Repo Update

---
+++ MySQL/Learning-SQL/Chp1/insert_FF_WT.sql
@@ -0,0 +1,6 @@
+INSERT INTO favorite_food (person_id, food)
+    VALUES (1, 'pizza');
+INSERT INTO favorite_food (person_id, food)
+    VALUES (1, 'cookies');
+INSERT INTO favorite_food (person_id, food)
+    VALUES (1, 'nachos');
---
+++ MySQL/Learning-SQL/Chp1/new_person.sql
@@ -0,0 +1,2 @@
+INSERT INTO person (person_id, fname, lname, eye_color, birth_date, street, city, state, country, postal_code) 
+VALUES (null, 'Susan', 'Smith', 'BL', '1975-11-02', '23 Maple St.', 'Arlington', 'VA', 'USA', '20220');
